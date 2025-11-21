17bb800b38e399a0b7b7077248a80cf156c08686
danis <danismagizov@gmail.com>
2025-11-21 19:40:42 +0300

    SL1-5 задача про время в космосе

---
+++ src/main/kotlin/lesson_5/lesson_5.kt
@@ -0,0 +1,9 @@
+package org.example.lesson_5
+
+fun main() {
+    val totalSeconds = 6480
+    val minutes = totalSeconds / 60
+    val hours = minutes / 60
+    val remainingMinutes = minutes % 60
+    println("0$hours:$remainingMinutes:00")
+}

4474f4a929d40c6c66aa89d3443932d597cf58a5
danis <danismagizov@gmail.com>
2025-11-22 14:19:28 +0300

    KS-2-1 средний балл

---
+++ src/main/kotlin/lesson_2/lesson_2_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    val result = (student1 + student2 + student3 + student4) / 4.0f
+    println(String.format("%.2f", result))
+}

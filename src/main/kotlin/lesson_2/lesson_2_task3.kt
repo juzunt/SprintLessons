30a6d6b717463c2aefb59a1504941c097d5dd8c7
danis <danismagizov@gmail.com>
2025-11-22 17:01:06 +0300

    KS-2-3 расписание поезда

---
+++ src/main/kotlin/lesson_2/lesson_2_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_2
+
+const val MINUTE_IN_HOUR = 60
+const val HOURS_IN_DAY = 24
+
+fun main() {
+    val departureHour = 9
+    val departureMinute = 39
+    val travelMinutes = 457
+
+    val departureTotalMinutes = departureHour * MINUTE_IN_HOUR + departureMinute // 579
+
+    val arrivalTotalMinutes = departureTotalMinutes + travelMinutes // 1036
+
+    val arrivalHour = (arrivalTotalMinutes / MINUTE_IN_HOUR) % HOURS_IN_DAY
+    val arrivalMinute = arrivalTotalMinutes % MINUTE_IN_HOUR
+
+    println(String.format("Время прибытия поезда: %02d:%02d", arrivalHour, arrivalMinute))
+}

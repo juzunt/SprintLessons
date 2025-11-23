fc34c57f21ec985c5442ae166e90acdf120e7de2
danis <danismagizov@gmail.com>
2025-11-23 12:21:22 +0300

    KS-3-1 приветствие

---
+++ src/main/kotlin/lesson_3/lesson_3_task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_3
+
+fun main() {
+    var greeting: String
+    val userName = "Марк"
+
+    greeting = "Доброе утро"
+    println("$greeting, $userName")
+
+    greeting = "Добрый вечер"
+    println("$greeting, $userName")
+}

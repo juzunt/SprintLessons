4bcb10c163f323f65d9486d8c4d4ab0b71be7cc8
danis <danismagizov@gmail.com>
2025-11-23 12:31:22 +0300

    KS-3-2 замужество и смена фамилии

---
+++ src/main/kotlin/lesson_3/lesson_3_task2.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_3
+
+fun main() {
+    val firstName = "Татьяна"
+    var lastName = "Андреева"
+    val middleName = "Сергеевна"
+    var age = 20
+
+    println("$lastName $firstName $middleName, $age лет")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("$lastName $firstName $middleName, $age года")
+}

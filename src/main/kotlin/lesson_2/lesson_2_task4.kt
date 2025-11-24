2aaa7c5a5f86cdbea432f713bc5d6d392eb2fdce
danis <danismagizov@gmail.com>
2025-11-24 10:00:34 +0300

    KS-2-4 доработал

--- src/main/kotlin/lesson_2/lesson_2_task4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_2
 
 fun main() {
-    val BUFF_PERCENT = 20.0f
+    val buffPercent = 20.0f
 
     val crystalOre = 7
     val ironOre = 11
 
-    val bonusCrystalOre = (crystalOre * BUFF_PERCENT / 100).toInt()
+    val bonusCrystalOre = (crystalOre * buffPercent / 100).toInt()
 
-    val bonusIronOre = (ironOre * BUFF_PERCENT / 100).toInt()
+    val bonusIronOre = (ironOre * buffPercent / 100).toInt()
 
     println("Кристальная руда: $bonusCrystalOre")
     println("Железная руда: $bonusIronOre")

422d8017d9b2f65075ae7596b0cc0d96497bea9c
danis <danismagizov@gmail.com>
2025-11-26 10:44:52 +0300

    KS-5-1 доработал

--- src/main/kotlin/lesson_5/lesson_5_task1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task1.kt
@@ -2,12 +2,16 @@ package org.example.lesson_5
 
 fun main() {
 
+    val a = 5
+    val b = 6
+    val sum = a + b
+
     println("Докажите, что вы не бот.")
-    print("Решите пример: 2 + 2 = ")
+    print("Решите пример: $a + $b = ")
 
     val verification = readln().toInt()
 
-    val result = if (verification == 4) {
+    val result = if (verification == sum) {
         "Добро пожаловать!"
     } else {
         "Доступ запрещен"
